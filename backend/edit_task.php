cd401a3567c9d9ff6835174c90873500d8d4cdf7
Luca <johannahmagadia.257@gmail.com>
2025-10-26 21:06:22 +0800

    created edit task backend
    created edit task backend (not yet connected to frontend)

--- backend/edit_task.php
+++ backend/edit_task.php
@@ -1,17 +1,18 @@
 <?php
-include_once ("db_connection.php");
+include_once ("db_connect.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $taskName = $_POST['taskName'];
     $taskDueDate = $_POST['due_date'];
     $taskPrio = $_POST['priority'];
     $taskStatus = $_POST['status'];
 
-    $sql = "INSERT INTO student_task_tracker (title, due_date, priority, status)
-            VALUES ('$taskName', '$taskDueDate', '$taskPrio', '$taskStatus')";
+    $sql = "UPDATE student_task_tracker
+            SET due_date = '$taskDueDate', priority = '$taskPrio', status = '$taskStatus'
+            WHERE title = '$taskName'"; 
 
     if (mysqli_query($conn, $sql)) {
-        echo "New task added.";
-        header('Location: index.html');
+        echo "Task updated.";
+        header('Location: inventory.php');
     } else {
         echo "Oops! Something went wrong. Please try again later.";
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
