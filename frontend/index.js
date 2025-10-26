91631d2f32e8c306805b05d69a780b1ab7524cad
Dana Alania <dana.p.alania@gmail.com>
2025-10-26 18:15:29 +0800

    added logic to show completed tasks

--- frontend/index.js
+++ frontend/index.js
@@ -2,16 +2,26 @@ function showAll() {
   document.getElementById("all").style.display = "block";
   document.getElementById("pending").style.display = "none";
   document.getElementById("inprogress").style.display = "none";
+  document.getElementById("completed").style.display = "none";
 }
 
 function showPending() {
   document.getElementById("all").style.display = "none";
   document.getElementById("pending").style.display = "block";
   document.getElementById("inprogress").style.display = "none";
+  document.getElementById("completed").style.display = "none";
 }
 
 function showInProgress() {
   document.getElementById("all").style.display = "none";
   document.getElementById("pending").style.display = "none";
   document.getElementById("inprogress").style.display = "block";
+  document.getElementById("completed").style.display = "none";
+}
+
+function showCompleted() {
+  document.getElementById("all").style.display = "none";
+  document.getElementById("pending").style.display = "none";
+  document.getElementById("inprogress").style.display = "none";
+  document.getElementById("completed").style.display = "block";
 }
