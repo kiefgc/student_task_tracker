7afa4ab3722595fae63cd43381a851e99edcb5d5
Dana Alania <dana.p.alania@gmail.com>
2025-10-26 18:12:44 +0800

    added logic to handle in progress tab

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,17 @@
 function showAll() {
   document.getElementById("all").style.display = "block";
   document.getElementById("pending").style.display = "none";
+  document.getElementById("inprogress").style.display = "none";
 }
 
 function showPending() {
   document.getElementById("all").style.display = "none";
   document.getElementById("pending").style.display = "block";
+  document.getElementById("inprogress").style.display = "none";
+}
+
+function showInProgress() {
+  document.getElementById("all").style.display = "none";
+  document.getElementById("pending").style.display = "none";
+  document.getElementById("inprogress").style.display = "block";
 }
