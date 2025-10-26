3c3da9f7c641c009faa3621c5474310d3af66806
Dana Alania <dana.p.alania@gmail.com>
2025-10-26 18:03:40 +0800

    switching between all tasks and pending tasks
    lacking ui in showing active tab

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+function showAll() {
+  document.getElementById("all").style.display = "block";
+  document.getElementById("pending").style.display = "none";
+}
+
+function showPending() {
+  document.getElementById("all").style.display = "none";
+  document.getElementById("pending").style.display = "block";
+}
